0cbe4b6653eccf548491b253c06f178fd39eeed6
unknown <avilarranz@gmail.com>
2026-09-10 09:45:03 +0200

    feat:added login register cart and profile nav structure to header

--- src/components/header/HeaderComponent.vue
+++ src/components/header/HeaderComponent.vue
@@ -7,6 +7,32 @@
             <img src="">
             <h1>Bella Vista</h1>
         </div>
+        <nav>
+            <ul>
+                <li>
+                    <router-link>
+                        Login
+                    </router-link>
+                </li>
+                <li>
+                    <router-link>
+                        Register
+                    </router-link>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <router-link>
+
+                    </router-link>
+                </li>
+                <li>
+                    <router-link>
+                        
+                    </router-link>
+                </li>
+            </ul>
+        </nav>
     </header>
 </template>
 
