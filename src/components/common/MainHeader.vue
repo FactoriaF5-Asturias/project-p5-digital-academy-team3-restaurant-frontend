604a91ec6d07844486b7e5ee66f035938793a045
unknown <avilarranz@gmail.com>
2026-09-11 09:42:45 +0200

    style:added background colors to header and header elements

--- src/components/common/MainHeader.vue
+++ src/components/common/MainHeader.vue
@@ -9,7 +9,7 @@
 </script>
 
 <template>
-    <header class="flex justify-between items-center p-14">
+    <header class="flex justify-between items-center p-14 bg-bg-body text-text-brand">
         <div>
             <RouterLink class="flex items-center gap-2" to="/home" aria-label="Ir a página principal">
                 <img class="h-7" :src="restaurantLogo" alt="">
@@ -31,16 +31,16 @@
             </ul>
             <ul class="flex items-center gap-2">
                 <li>
-                    <RouterLink class="relative rounded-full h-7 w-7 flex items-center justify-center" to="/cart" aria-label="Ir al carrito">
-                        <img class="h-5" :src="cartIcon" alt="">
-                        <span class="absolute -top-1 -right-1 w-4 h-4 rounded-full text-xs font-semibold flex items-center justify-center">
+                    <RouterLink class="relative rounded-full h-9 w-9 flex items-center justify-center bg-bg-container-high p-2" to="/cart" aria-label="Ir al carrito">
+                        <img class="h-8" :src="cartIcon" alt="">
+                        <span class="absolute -top-1 -right-1 w-5 h-5 rounded-full text-xs font-semibold bg-bg-brand text-text-on-brand flex items-center justify-center">
                             {{ counter }}
                         </span>
                     </RouterLink>
                 </li>
                 <li>
-                    <RouterLink class="rounded-full h-7 w-7 flex items-center justify-center" to="/profile" aria-label="Ir al perfil">
-                        <img class="h-5" :src="profileIcon" alt="">
+                    <RouterLink class="relative rounded-full h-9 w-9 flex items-center justify-center bg-bg-brand p-2" to="/profile" aria-label="Ir al perfil">
+                        <img class="h-4" :src="profileIcon" alt="">
                     </RouterLink>
                 </li>
             </ul>
