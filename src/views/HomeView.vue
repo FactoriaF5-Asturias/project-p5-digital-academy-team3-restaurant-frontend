22069ef3b172395b9625d837137b44d2556d2744
unknown <avilarranz@gmail.com>
2026-09-14 11:37:30 +0200

    feat:added header and footer components to homeview

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import MainHeader from '../components/common/MainHeader.vue'
+import MainFooter from '../components/common/MainFooter.vue';
+</script>
+
+<template>
+    <MainHeader />
+    <MainFooter />
+</template>
