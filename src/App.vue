aca5f07cf311ed751d45b30c11dd2cae8d1e2cf6
unknown <avilarranz@gmail.com>
2026-09-14 11:34:16 +0200

    chore:created home view file

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <script setup>
-import MainHeader from './components/common/MainHeader.vue';
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <HelloWorld />
-  <MainHeader />
 </template>
