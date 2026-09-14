0b14187acfb7ab7fb4358da3219626ad065812cd
unknown <avilarranz@gmail.com>
2026-09-14 12:23:21 +0200

    style:changed footer home button size

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup>
+import MainFooter from './components/common/MainFooter.vue';
+import MainHeader from './components/common/MainHeader.vue';
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <HelloWorld />
+  <MainHeader />
+  <MainFooter />
 </template>
--- src/components/common/MainFooter.vue
+++ src/components/common/MainFooter.vue
@@ -7,8 +7,8 @@
     <footer class="flex justify-between items-center px-10 py-5 bg-bg-container text-text-brand shadow-sm">
         <div>
             <RouterLink class="flex items-center gap-2" to="/home" aria-label="Ir a página principal">
-                <img class="h-10" :src="restaurantLogo" alt="">
-                <span class="font-['Playfair-Display'] text-3xl font-bold">Bella Vita</span>
+                <img class="h-7" :src="restaurantLogo" alt="">
+                <span class="font-['Playfair-Display'] text-xl font-bold">Bella Vita</span>
             </RouterLink>
         </div>
         <span>
