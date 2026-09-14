9b17fdc3974fada9265a0f3b4786e8c993b6dde2
unknown <avilarranz@gmail.com>
2026-09-14 15:11:31 +0200

    feat:merge with feat/hero

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import MainHeader from '../components/common/MainHeader.vue'
 import MainFooter from '../components/common/MainFooter.vue';
+import HeroSection from '../components/home/HeroSection.vue';
 </script>
 
 <template>
     <MainHeader />
+    <HeroSection />
     <MainFooter />
 </template>
