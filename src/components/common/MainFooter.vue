c4d8f58d29e5296a976a7e55a343e5a9494938c8
unknown <avilarranz@gmail.com>
2026-09-14 11:23:16 +0200

    feat:added home button to footer copied from header

---
+++ src/components/common/MainFooter.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import restaurantLogo from '../../assets/bella_vita_logo.png'
+
+</script>
+
+<template>
+    <footer class="flex justify-between items-center px-10 py-5 bg-bg-container text-text-brand shadow-sm">
+        <div>
+            <RouterLink class="flex items-center gap-2" to="/home" aria-label="Ir a página principal">
+                <img class="h-10" :src="restaurantLogo" alt="">
+                <span class="font-['Playfair-Display'] text-3xl font-bold">Bella Vita</span>
+            </RouterLink>
+        </div>
+        
+    </footer>
+</template>
