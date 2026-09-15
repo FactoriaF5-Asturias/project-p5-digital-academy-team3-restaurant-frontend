f393fe3f560e7501706731e8120479a53bb6103b
Jose Ángel Peña Díaz <penadiazjoseangel@gmail.com>
2026-09-15 14:38:20 +0200

    feat: Template for EmptyCart
    Skeleton template for the empty cart component

---
+++ src/components/EmptyCart.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="empty-cart">
+        <img src="../assets/cart.svg" alt="cart">
+        <h2>Tu cesta está vacía</h2>
+        <p class="empty-cart_message">Parece que aún no has añadido ningún producto. Explora el catálogo y encuentra algo que te apetezca.</p>
+
+        <button
+            type="button"
+            class="empty-cart_action"
+            @click="$emit('continue-shopping')"
+        >
+            Volver al catálogo
+        </button>
+    </div>
+</template>
