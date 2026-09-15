e75f81735919e12b07df0ca2e6ced6682fdce6f0
Hanna <hannafrolova14@gmail.com>
2026-09-15 12:40:41 +0200

    feat: add home contact section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,10 +2,12 @@
 import MainHeader from '../components/common/MainHeader.vue'
 import MainFooter from '../components/common/MainFooter.vue';
 import HeroSection from '../components/home/HeroSection.vue';
+import ContactSection from '../components/home/ContactSection.vue';
 </script>
 
 <template>
     <MainHeader />
     <HeroSection />
+    <ContactSection />
     <MainFooter />
 </template>
