1400843d536cc3d760363bb4ce86d8a2ac213140
unknown <avilarranz@gmail.com>
2026-09-15 12:39:01 +0200

    feat:created base product card html structure

---
+++ src/components/home/ProductCard.vue
@@ -0,0 +1,33 @@
+<script setup>
+</script>
+
+<template>
+    <article>
+        <img src="" alt="">
+        <h3>
+
+        </h3>
+        <p>
+
+        </p>
+        <div>
+            <div>
+                <button type="button">
+
+                </button>
+                <span>
+
+                </span>
+                <button type="button">
+
+                </button>
+            </div>
+            <p>
+                
+            </p>
+        </div>
+        <button>
+
+        </button>
+    </article>
+</template>
