c3d4cc71b33d850c666b173032a2f5adade4cd9e
unknown <avilarranz@gmail.com>
2026-09-15 14:14:31 +0200

    feat:added other props

--- src/components/home/ProductCard.vue
+++ src/components/home/ProductCard.vue
@@ -5,6 +5,26 @@ const props = defineProps({
     id: {
         type: Number,
         required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    imageUrl: {
+        type: String,
+        required: true,
     }
 })
 
