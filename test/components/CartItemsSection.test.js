82c7a1ef6c6c1e9740b3ada72c087ee7936c2412
Jose Ángel Peña Díaz <penadiazjoseangel@gmail.com>
2026-09-24 12:45:17 +0200

    test: CartItemsSection renders a CartItem pero item received

---
+++ test/components/CartItemsSection.test.js
@@ -0,0 +1,32 @@
+import { mount } from '@vue/test-utils'
+import { describe, it, expect } from 'vitest'
+import CartItemsSection from '../../src/components/CartItemsSection.vue'
+import CartItem from '../../src/components/CartItem.vue'
+
+function makeItems() {
+    return [
+        {
+            id: 1,
+            name: 'Pizza',
+            description: 'Con queso',
+            price: 9,
+            quantity: 1,
+            image: '/a.jpg'
+        },
+        {
+            id: 2,
+            name: 'Pasta',
+            description: 'Con tomate',
+            price: 8,
+            quantity: 2,
+            image: 'b.jpj'
+        }
+    ]
+}
+
+describe('CartItemsSection', () => {
+    it('renders a CartItem per item received', () => {
+        const wrapper = mount(CartItemsSection, { props: { items: makeItems() } })
+        expect(wrapper.findAllComponents(CartItem)).toHaveLength(2)
+    })
+})
