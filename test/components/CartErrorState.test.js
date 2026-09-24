2bda7f11b120f6849819394ff4ac971e65b3f9d8
Jose Ángel Peña Díaz <penadiazjoseangel@gmail.com>
2026-09-24 11:30:09 +0200

    feat: Default message tested

--- test/components/CartErrorState.test.js
+++ test/components/CartErrorState.test.js
@@ -7,4 +7,9 @@ describe('CartErrorState', () => {
         const wrapper = mount(CartErrorState)
         expect(wrapper.get('.cart-error').attributes('role')).toBe('alert')
     })
+
+    it('uses default message while "prop" message is not being thrown', () => {
+        const wrapper = mount(CartErrorState)
+        expect(wrapper.get('.cart-error_message').text()).toBe('No se pudo cargar tu cesta. Inténtalo de nuevo.')
+    })
 })
