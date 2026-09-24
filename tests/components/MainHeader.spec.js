0c566e61ada321d3505bfc48977c5b56ee7790b5
unknown <avilarranz@gmail.com>
2026-09-24 09:54:05 +0200

    feat:added shallowmount as mountheader function to avoid repetition

--- tests/components/MainHeader.spec.js
+++ tests/components/MainHeader.spec.js
@@ -4,8 +4,8 @@ import MainHeader from "../../src/components/common/MainHeader.vue";
 
 describe('MainHeader', () => {
 
-    it('renderiza el texto del boton Home', () => {
-        const wrapper = shallowMount(MainHeader,{
+    function mountHeader() {
+        return shallowMount(MainHeader, {
             global: {
                 stubs: {
                     RouterLink: {
@@ -14,19 +14,15 @@ describe('MainHeader', () => {
                 }
             }
         })
+    }
+
+    it('renderiza el texto del boton Home', () => {
+        const wrapper = mountHeader()
         expect(wrapper.text()).toContain('Giacobello')
     })
 
     it('renderiza la imagen del boton Home', () => {
-        const wrapper = shallowMount(MainHeader,{
-            global: {
-                stubs: {
-                    RouterLink: {
-                        template: '<a><slot /></a>'
-                    }
-                }
-            }
-        })
+        const wrapper = mountHeader()
         expect(wrapper.find('img').exists()).toBe(true)
     })
 
