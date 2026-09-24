550ca29e53293318c6e1dbebae7d726ef5ec82ec
Jose Ángel Peña Díaz <penadiazjoseangel@gmail.com>
2026-09-24 14:38:14 +0200

    test: Rendered three delivery options

--- test/components/CartSummary.test.js
+++ test/components/CartSummary.test.js
@@ -21,4 +21,16 @@ describe('CartSummary', () => {
         expect(lines[1].get('dd').text()).toBe('2,50\u00A0€')
         expect(wrapper.get('.cart-summary_total-amount').text()).toContain('22,50\u00A0€')
     })
+
+    it('renders the three delivery options', () => {
+        const wrapper = mount(CartSummary, { props: makeProps() })
+        const options = wrapper.findAll('.cart-summary_delivery-option')
+
+        expect(options).toHaveLength(3)
+        expect(options.map(o => o.text())).toEqual([
+            'Comer en el restaurante',
+            'Para llevar',
+            'Envío a domicilio'
+        ])
+    })
 })
